7fea91052177d500a7544cc6a50ed98adc65492e
tobi110289 <tobias.feltes@web.de>
2021-11-10 23:38:07 +0100

    fix(app): added Head meta viewport

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,17 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "theme-ui";
 import theme from "../theme";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1.0"
+        ></meta>
+      </Head>
       <Component {...pageProps} />
     </ThemeProvider>
   );
