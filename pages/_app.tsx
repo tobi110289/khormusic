e0d554942ac00d4eb1b49095157411332850d67e
tobi110289 <tobias.feltes@web.de>
2021-11-12 13:42:16 +0100

    refactor(app): removed CSP

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,13 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "theme-ui";
-import { NextStrictCSP } from "next-strict-csp";
 import theme from "../theme";
 import Head from "next/head";
-
-const HeadCSP = process.env.NODE_ENV === "production" ? NextStrictCSP : Head;
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
-      <HeadCSP>
-        {process.env.NODE_ENV === "production" && (
-          <meta httpEquiv="Content-Security-Policy" />
-        )}
+      <Head>
         <title>KHOR MUSIC</title>
         <meta
           name="viewport"
@@ -23,7 +17,7 @@ function MyApp({ Component, pageProps }: AppProps) {
           name="description"
           content="An official online resource containing everything we, Berlin based band KHOR, have ever done, more or less. Videos, music, images, websites, instagram, email and assorted ephemeral materials."
         ></meta>
-      </HeadCSP>
+      </Head>
       <Component {...pageProps} />
     </ThemeProvider>
   );
