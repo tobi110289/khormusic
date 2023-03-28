7303a7e4492692c277eff462ec7916b0e07cf4aa
tobi110289 <tobias.feltes@web.de>
2023-03-29 00:35:38 +0200

    feat(video): deleted cc vids

--- pages/videos.tsx
+++ pages/videos.tsx
@@ -35,44 +35,6 @@ const Videos: NextPage = () => {
         }}
       >
         <iframe
-          src="https://player.vimeo.com/video/802275054?h=7d3f2279d9&title=0&byline=0&portrait=0"
-          style={{
-            position: "absolute",
-            top: 0,
-            left: 0,
-            width: "100%",
-            height: "100%",
-            border: "0px",
-          }}
-          allow="autoplay; fullscreen; picture-in-picture"
-          allowFullScreen
-        ></iframe>
-      </div>
-      <div
-        sx={{
-          variant: "containers.vimeo",
-        }}
-      >
-        <iframe
-          src="https://player.vimeo.com/video/802272844?h=2c404787ae&title=0&byline=0&portrait=0"
-          style={{
-            position: "absolute",
-            top: 0,
-            left: 0,
-            width: "100%",
-            height: "100%",
-            border: "0px",
-          }}
-          allow="autoplay; fullscreen; picture-in-picture"
-          allowFullScreen
-        ></iframe>
-      </div>
-      <div
-        sx={{
-          variant: "containers.vimeo",
-        }}
-      >
-        <iframe
           src="https://player.vimeo.com/video/642797492?h=f7098ef26a&title=0&byline=0&portrait=0"
           style={{
             position: "absolute",
